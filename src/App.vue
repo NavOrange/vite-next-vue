c3a2f7e5b04d9abb4a991f26676e2cda47439df8
albert <fangniu@live.com>
2020-08-11 16:51:05 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <router-view></router-view>
+  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  /* components: {
+    HelloWorld
+  } */
+}
+</script>
